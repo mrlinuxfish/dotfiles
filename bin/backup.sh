cd49c25a109e768794f2cb4893c1b3636939199a
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2017-01-20 06:24:03 -0700

    Add install scripts

---
+++ bin/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## This script is called by install.sh to backup old dotfiles to $dotfiles/backup/ ##
+
+echo "backing up dotfiles..."
---
+++ bin/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## The purpose of this script is to backup old dotfiles and install dotfiles from the dotfiles directory ##
+## this script is in progress ##
+
+echo "the script is not finished :("
+
+# run the backup script
+#Backup.sh
