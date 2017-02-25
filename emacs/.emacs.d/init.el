52f1b16b79b0bc14727f3de1e1de613e34840ed7
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2017-02-24 23:52:05 -0700

    Update init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,3 +12,18 @@
 ;; Put custom-set-variables in custom.el
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;;;;org-mode configuration
+;; Enable org-mode
+(require 'org)
+
+;; Mobile org
+;; Set location of Org files
+(setq org-directory "~/org")
+;; Set name of file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; Set dropbox directory
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
