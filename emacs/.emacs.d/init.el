1782b40e8a2029bc39485664bc935afff4d859c1
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2017-02-24 23:22:56 -0700

    Move custom file to .emacs.d/custom.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,15 +8,7 @@
 
 (setq package-enable-at-startup nil)
 (package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-mobile-sync helm org org-drill-table))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; Put custom-set-variables in custom.el
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
