6484bae72eee64fd61397444a12181a35cad9550
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2018-03-06 17:11:13 -0700

    Initial commit

---
+++ .scripts/cst.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xinput set-prop "CST CST USB UNITRAC" "libinput Middle Emulation Enabled" 1
+xinput set-prop "CST CST USB UNITRAC" "libinput Accel Speed" .6
+xinput set-prop "CST CST USB UNITRAC" "libinput Scroll Method Enabled" 0, 0, 1
