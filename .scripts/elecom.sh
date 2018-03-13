19d9ccbee2fd756d9acabd14c780a0e1969fb1e4
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2018-03-12 18:51:05 -0600

    Change button mapping on trackball

--- .scripts/elecom.sh
+++ .scripts/elecom.sh
@@ -4,4 +4,3 @@ Mouse=`xinput list |grep -i ELECOM | awk -F= '{ print $2}'| awk '{print $1}'| aw
 
 xinput set-prop $Mouse "libinput Middle Emulation Enabled" 1
 xinput set-prop $Mouse "libinput Scroll Method Enabled" 0, 0, 1
-xinput set-button-map $Mouse 1 2 3 4 5 6 7 8 9 2 11 12
