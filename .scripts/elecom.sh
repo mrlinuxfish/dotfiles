2f53a713cc5f67363b4f3bced90c68f18e444f90
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2018-03-13 22:25:09 -0600

    Adjust accel speed on trackball

--- .scripts/elecom.sh
+++ .scripts/elecom.sh
@@ -4,3 +4,4 @@ Mouse=`xinput list |grep -i ELECOM | awk -F= '{ print $2}'| awk '{print $1}'| aw
 
 xinput set-prop $Mouse "libinput Middle Emulation Enabled" 1
 xinput set-prop $Mouse "libinput Scroll Method Enabled" 0, 0, 1
+xinput set-prop $Mouse "libinput Accel Speed" 0
