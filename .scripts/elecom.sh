2b58dd3cfb970d3701dce476cf4b8f46b22a4e8e
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2018-03-21 18:39:40 -0600

    Adjust pointer sensitivity and acceleration

--- .scripts/elecom.sh
+++ .scripts/elecom.sh
@@ -4,4 +4,5 @@ Mouse=`xinput list |grep -i ELECOM | awk -F= '{ print $2}'| awk '{print $1}'| aw
 
 xinput set-prop $Mouse "libinput Middle Emulation Enabled" 1
 xinput set-prop $Mouse "libinput Scroll Method Enabled" 0, 0, 1
-xinput set-prop $Mouse "libinput Accel Speed" .2
+xinput set-prop $Mouse "libinput Accel Speed" .9
+xinput set-prop $Mouse "Coordinate Transformation Matrix" .65 0 0 0 .65 0 0 0 3
