cf535655cb8cff53f705023127aba229b8c33185
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2018-04-03 19:33:55 -0600

    Fix sensitivity to be more correct

--- .scripts/huion.sh
+++ .scripts/huion.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xinput set-prop 'HUION 420 Pen' --type=float "Coordinate Transformation Matrix"  2.39 0 -.2 0 2.39 -.1 0 0 1
+xinput set-prop 'HUION 420 Pen' --type=float "Coordinate Transformation Matrix"  2.88 0 -.2 0 2.88 -.1 0 0 1
