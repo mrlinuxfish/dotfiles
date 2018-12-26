c80a0608609ac6caeeffca4c92d09c6371768e5f
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2018-12-26 15:59:25 -0700

    initial commit

---
+++ .scripts/huion-laptop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+Pen=`xinput list |grep -i HUION\ 420\ Pen | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
+
+xinput set-prop --type=float $Pen 'Coordinate Transformation Matrix' 1.82 0 -.2 0 1.82 -.1 0 0 1
