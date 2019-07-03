911b11901a0513f19f7de0d89e2d14f44a3b6bec
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2019-07-03 14:07:49 -0600

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,11 +4,11 @@
 ;; Bootstrap 'use-package'
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-
+ 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
