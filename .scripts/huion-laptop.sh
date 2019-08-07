a468e676b426ec6e9c4e51e595b8da21efd89511
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2019-08-07 10:48:51 -0600

    update

--- .scripts/huion-laptop.sh
+++ .scripts/huion-laptop.sh
@@ -2,4 +2,4 @@
 
 Pen=`xinput list |grep -i HUION\ 420\ Pen | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
 
-xinput set-prop --type=float $Pen 'Coordinate Transformation Matrix' 1.82 0 -.2 0 1.82 -.1 0 0 1
+xinput set-prop --type=float $Pen 'Coordinate Transformation Matrix' 2.3 0 -.2 0 2.3 -.8 0 0 1
