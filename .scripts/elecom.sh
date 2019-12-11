631170a35a127a35cfcf0f0656eafb3ab03f8e74
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2019-12-10 17:08:18 -0700

    Update config
    - Turn off middle scroll (so some programs work better)
    - Add dunst to startup

--- .scripts/elecom.sh
+++ .scripts/elecom.sh
@@ -3,6 +3,6 @@
 Mouse=`xinput list |grep -i ELECOM | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
 
 xinput set-prop $Mouse "libinput Middle Emulation Enabled" 1
-xinput set-prop $Mouse "libinput Scroll Method Enabled" 0, 0, 1
+xinput set-prop $Mouse "libinput Scroll Method Enabled" 0, 0, 0
 xinput set-prop $Mouse "libinput Accel Speed" .85
 xinput set-prop $Mouse "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 2.6
