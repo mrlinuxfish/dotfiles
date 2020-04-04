267cef823fb4ec871a44c50b7852555ea34fe551
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-04-03 20:34:53 -0600

    Update tablet position script
    - Remove unnecessary backup file
    - Add comments to make editing the script easier in the future

--- .scripts/huion-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-Pen=`xinput list |grep -i HUION\ 420\ Pen | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
-
-xinput set-prop --type=float $Pen 'Coordinate Transformation Matrix' 1.22 0 -.2 0 1.22 -.1 0 0 1
--- .scripts/huion.sh
+++ .scripts/huion.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Uniform x/y size adjustment (old size 1.35)
+size=1.7
+
+# x/y offset
+x_offset=-.2
+y_offset=-.1
+
 Pen=`xinput list |grep -i HUION\ 420\ Pen | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
 
-xinput set-prop --type=float $Pen 'Coordinate Transformation Matrix' 1.35 0 -.2 0 1.35 -.1 0 0 1
+xinput set-prop --type=float $Pen 'Coordinate Transformation Matrix' $size 0 $x_offset 0 $size $y_offset 0 0 1
