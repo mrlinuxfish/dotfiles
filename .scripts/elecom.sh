4867c61b2fb78a997aec029b945eb2f94dd56256
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-04-04 18:22:32 -0600

    Fix script
    - Iteratively apply settings to all elecom pointing devices

--- .scripts/elecom.sh
+++ .scripts/elecom.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-Mouse=`xinput list |grep -i ELECOM | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
-
-xinput set-prop $Mouse "libinput Middle Emulation Enabled" 1
-xinput set-prop $Mouse "libinput Scroll Method Enabled" 0, 0, 1
-xinput set-prop $Mouse "libinput Accel Speed" .85
-xinput set-prop $Mouse "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 2.6
+# Find all elecom pointer devices and iteratively apply the correct settings
+# This will attempt to apply incorrect settings to some devices but this shouldn't be a problem
+for Trackball in `xinput list |awk -F= '/ELECOM.*pointer/ { print $2}' |awk '{ print $1}'`
+do
+    echo $Trackball
+xinput set-prop $Trackball "libinput Middle Emulation Enabled" 1
+xinput set-prop $Trackball "libinput Scroll Method Enabled" 0, 0, 1
+xinput set-prop $Trackball "libinput Accel Speed" .85
+xinput set-prop $Trackball "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 2.6
+done
