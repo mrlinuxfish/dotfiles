4440209d87c6474c0897e5b8d5396d19f5d31c10
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-06-27 20:09:56 -0600

    Move configuration to xorg module

--- .scripts/elecom.sh
+++ .scripts/elecom.sh
@@ -2,11 +2,11 @@
 
 # Find all elecom pointer devices and iteratively apply the correct settings
 # This will attempt to apply incorrect settings to some devices but this shouldn't be a problem
-for Trackball in `xinput list |awk -F= '/ELECOM.*pointer/ { print $2}' |awk '{ print $1}'`
-do
-    echo $Trackball
-xinput set-prop $Trackball "libinput Middle Emulation Enabled" 1
-xinput set-prop $Trackball "libinput Scroll Method Enabled" 0, 0, 1
-xinput set-prop $Trackball "libinput Accel Speed" .85
-xinput set-prop $Trackball "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 2.6
-done
+# for Trackball in `xinput list |awk -F= '/ELECOM.*pointer/ { print $2}' |awk '{ print $1}'`
+# do
+#     echo $Trackball
+# xinput set-prop $Trackball "libinput Middle Emulation Enabled" 1
+# xinput set-prop $Trackball "libinput Scroll Method Enabled" 0, 0, 1
+# xinput set-prop $Trackball "libinput Accel Speed" .85
+# xinput set-prop $Trackball "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 2.6
+# done
